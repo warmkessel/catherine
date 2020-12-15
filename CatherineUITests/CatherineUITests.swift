2918f929cd086f06641aa70317b77b066b87cc63
James <jr@warmkessel.com>
2020-12-15 11:31:11 -0800

    Initial

--- CatherineUITests/CatherineUITests.swift
+++ CatherineUITests/CatherineUITests.swift
@@ -22,15 +22,60 @@ class CatherineUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testReportGood() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
-        app.launch()
+            app.launch()
+            let reportShowButton  = app.tabBars["Tab Bar"].buttons["Report"]
+            XCTAssertTrue(reportShowButton.waitForExistence(timeout: 5))
+            reportShowButton.tap()
+            XCTAssertTrue(app.buttons["good"].waitForExistence(timeout: 5))
+            app.staticTexts["Good"].tap()
+            XCTAssertTrue(app.buttons["respond"].waitForExistence(timeout: 5))
+            app.buttons["respond"].tap()
+            XCTAssertTrue(app.buttons["poor"].waitForExistence(timeout: 5))
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    func testReportPoor() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+            app.launch()
+            let reportShowButton  = app.tabBars["Tab Bar"].buttons["Report"]
+            XCTAssertTrue(reportShowButton.waitForExistence(timeout: 5))
+            reportShowButton.tap()
+            XCTAssertTrue(app.buttons["poor"].waitForExistence(timeout: 5))
+            app.staticTexts["Poor"].tap()
+            XCTAssertTrue(app.buttons["respond"].waitForExistence(timeout: 5))
+            app.buttons["respond"].tap()
+            XCTAssertTrue(app.buttons["poor"].waitForExistence(timeout: 5))
 
+    }
+    func testSlideShow() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+            app.launch()
+        let slideShowButton  = app.tabBars["Tab Bar"].buttons["Slide Show"]
+        XCTAssertTrue(slideShowButton.waitForExistence(timeout: 5))
+        slideShowButton.tap()
+        XCTAssertTrue(app.buttons["speaker"].waitForExistence(timeout: 5))
+        app.buttons["speaker"].tap()
+        XCTAssertTrue(app.buttons["mute"].waitForExistence(timeout: 5))
+        app.buttons["mute"].tap()
+    }
+    func testSelfie() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+            app.launch()
+        let selfieShowButton  = app.tabBars["Tab Bar"].buttons["Selfie"]
+        XCTAssertTrue(selfieShowButton.waitForExistence(timeout: 5))
+        selfieShowButton.tap()
+        XCTAssertTrue(app/*@START_MENU_TOKEN@*/.images["person"]/*[[".images[\"recenter\"]",".images[\"person\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.waitForExistence(timeout: 5))
+        app/*@START_MENU_TOKEN@*/.images["person"]/*[[".images[\"recenter\"]",".images[\"person\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.images["camera.circle"].waitForExistence(timeout: 5))
+        app.images["camera.circle"].tap()
+        //app.alerts["“Catherine” Would Like to Add to your Photos"].scrollViews.otherElements.buttons["OK"].tap()
+        
+    }
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
